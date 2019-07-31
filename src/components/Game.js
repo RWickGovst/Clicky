744c60a348f8e6d55d1246693eccafa232361bf6
Triumph <archie200@icloud.com>
2019-07-31 15:00:25 -0500

    squares displayed

---
+++ src/components/Game.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Board from './Board';
+
+export class Game extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isNext: true,
+            stepNumber: 0,
+            history: [
+                {squares: Array(12).fill(null)}
+            ]
+        }  
+    }
+    render() {
+        const history = this.state.history;
+        const current = history[this.state.stepNumber];
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board onClick={(i)=>this.onClick[i]}
+                    squares={current.squares} />
+                </div>
+            </div>
+        );
+    }
+}
+export default Game;
